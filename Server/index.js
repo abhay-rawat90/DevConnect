5380dd422e293e0f6bd7a4b65cc678ae1ad97db3
Abhay Rawat <abhayrawat170@gmail.com>
2025-12-16 20:02:51 +0530

    Server Changes

--- Server/index.js
+++ Server/index.js
@@ -15,15 +15,22 @@ const Message = require('./models/Message');
 const app = express();
 const server = http.createServer(app);
 
+app.use(express.json());
+app.use(cors({
+  origin: "https://dev-connect-six-ruddy.vercel.app", // EXACT URL, no trailing slash
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
 const io = new Server(server, {
-    cors: {
-        origin: process.env.CLIENT_URL || "http://localhost:5173",
-        methods: ["GET","POST"]
-    }
+  cors: {
+    origin: "https://dev-connect-six-ruddy.vercel.app" || "http://localhost:5173", 
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
 
-app.use(cors());
-app.use(express.json());
+
 
 app.use('/api/auth', authRoutes);
 app.use("/api/users", userRoutes);
